f9c2e64dbe3c5ea3fd01543f4f2e4b1022277ed6
Altair J. Fernandes <tatosjb@gmail.com>
2021-09-21 16:09:35 -0300

    fix jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   moduleNameMapper: {
-    '\\.(css|scss)$': 'identity-obj-proxy'
+    '\\.(css|scss)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|svg)$': '<rootDir>/__mocks__/fileMock.js',
   },
   setupFilesAfterEnv: ['<rootDir>/jest-setup.js']
 }
